8a38d537ff569b04ec5c157ff844509f62e4898d
Chris Shyi <chris.shyi@mail.utoronto.ca>
2018-08-15 21:43:18 -0400

    Initial commit

---
+++ src/Tokenizer.java
@@ -0,0 +1,46 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Tokenizer {
+
+    // TODO: which class should be responsible for closing the source file?
+    private File sourceFile;
+    private BufferedReader reader;
+    private String nextToken;
+    private String[] currentLine;
+    private int currentIndex; // keeps track of where the Tokenizer is at in the current line
+
+    public Tokenizer(File sourceFile) throws IOException {
+        this.sourceFile = sourceFile;
+        this.reader = new BufferedReader(new FileReader(sourceFile));
+        this.nextToken = "";
+    }
+
+    public boolean hasNextToken() throws IOException {
+        // what about lines that start with '//'
+        if (currentLine == null || (this.currentIndex == this.currentLine.length - 1)
+            || (this.currentLine[this.currentIndex].startsWith("//"))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+
+            }
+            if (line == null) {
+                return false;
+            }
+            this.currentLine = line.split("\\s");
+            this.currentIndex = 0;
+        }
+        return true;
+    }
+
+    public String getNextToken() {
+        this.nextToken = this.currentLine[this.currentIndex];
+        this.currentIndex++;
+        return this.nextToken;
+    }
+
+
+
+}
